d8facc135d750d6d6e3b948d6155c2b576f777f0
ianmeigh <ianmeigh@googlemail.com>
2022-10-20 18:45:53 +0100

    refactor: change NavLink to Link

--- src/pages/auth/RegistrationForm.jsx
+++ src/pages/auth/RegistrationForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
+import axios from "axios";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/AuthForms.module.css";
 import btnStyles from "../../styles/Buttons.module.css";
@@ -14,6 +15,7 @@ export default function RegistrationForm() {
     isSeller: false,
   });
   const { username, password1, password2, isSeller } = registrationData;
+  const history = useHistory();
 
   const handleChange = (event) => {
     setRegistrationData({
@@ -28,6 +30,17 @@ export default function RegistrationForm() {
       [event.target.name]: event.target.checked,
     });
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(registrationData);
+    try {
+      await axios.post("/dj-rest-auth/registration/", registrationData);
+      history.push("/signin");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto mx-auto py-2 p-md-2" md={8} lg={6}>
@@ -37,11 +50,11 @@ export default function RegistrationForm() {
           <h1>Sign Up</h1>
           <p className="mt-2 mb-4">
             Already have an account?
-            <NavLink className="ps-1" to="/signin">
+            <Link className="ps-1" to="/signin">
               Sign in here.
-            </NavLink>
+            </Link>
           </p>
-          <Form className="align-self-start w-100">
+          <Form className="align-self-start w-100" onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="username">
               <Form.Label className="d-none">Username</Form.Label>
               <Form.Control
@@ -81,13 +94,13 @@ export default function RegistrationForm() {
                 onChange={handleChecked}
               />
             </Form.Group>
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Primary}`}
+              type="submit"
+            >
+              Sign Up
+            </Button>
           </Form>
-          <Button
-            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Primary}`}
-            type="submit"
-          >
-            Sign Up
-          </Button>
         </Container>
       </Col>
     </Row>
