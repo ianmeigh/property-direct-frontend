93c4500417baa10e5f028fcfc27757d69ceb8a51
ianmeigh <ianmeigh@googlemail.com>
2022-10-20 14:09:52 +0100

    feat: add registration form, styles and app route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
+import RegistrationForm from "./pages/auth/RegistrationForm";
 
 function App() {
   return (
@@ -16,11 +17,7 @@ function App() {
             path="/signin"
             render={() => <h1>Sign In Page Placeholder</h1>}
           />
-          <Route
-            exact
-            path="/signup"
-            render={() => <h1>Sign Up Page Placeholder</h1>}
-          />
+          <Route exact path="/signup" render={() => <RegistrationForm />} />
           <Route render={() => <p>Page Not Found Placeholder</p>} />
         </Switch>
       </Container>
---
+++ src/pages/auth/RegistrationForm.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+import { Col, Container, Row, Button, Form } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+import styles from "../../styles/AuthForms.module.css";
+
+export default function RegistrationForm() {
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto mx-auto py-2 p-md-2" md={8} lg={6}>
+        <Container
+          className={`${styles.FormContainer} p-4 d-flex flex-column align-items-center rounded`}
+        >
+          <h1>Sign Up</h1>
+          <p className="mt-2 mb-4">
+            Already have an account?
+            <NavLink className="ps-1" to="/signin">
+              Sign in here.
+            </NavLink>
+          </p>
+          <Form className="align-self-start w-100">
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter a username"
+                name="username"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password1">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter a password"
+                name="password1"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password2">
+              <Form.Label className="d-none">Confirm password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm password"
+                name="password2"
+              />
+            </Form.Group>
+            <Form.Group className="my-3" controlId="checkbox">
+              <Form.Check type="checkbox" label="Are you selling property?" />
+            </Form.Group>
+          </Form>
+          <Button variant="primary" type="submit">
+            Sign Up
+          </Button>
+        </Container>
+      </Col>
+    </Row>
+  );
+}
