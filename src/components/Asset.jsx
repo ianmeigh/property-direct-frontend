da32c77fec3be9cc40dbabb559c4016b943261c4
ianmeigh <ianmeigh@googlemail.com>
2022-10-25 20:11:38 +0100

    feat: add spinner/loading animation and prop to Asset.jsx

--- src/components/Asset.jsx
+++ src/components/Asset.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
+import { Spinner } from "react-bootstrap";
+
 import styles from "../styles/Asset.module.css";
 
 // CREDIT: Adapted from Code Institute Moments Tutorial Project
 // URL:    https://github.com/Code-Institute-Solutions/moments
 
-const Asset = ({ upload, message }) => {
+const Asset = ({ spinner, upload, message }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
+      {spinner && <Spinner animation="border" />}
       {upload && (
         <span className="d-flex flex-column justify-content-center">
           <i className="fas fa-upload fa-3x"></i>
