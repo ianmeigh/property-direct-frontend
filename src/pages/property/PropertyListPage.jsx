ec95b2dc1a016a1559ee8561f28fe39d6f07ed82
ianmeigh <ianmeigh@googlemail.com>
2022-10-26 23:54:50 +0100

    feat: display property component for each result fetched from the API
    Asset spinner is displayed until API results are fetched, it is then
    replaced by the property component.

--- src/pages/property/PropertyListPage.jsx
+++ src/pages/property/PropertyListPage.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import { useLocation } from "react-router-dom";
 
 import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import PropertyDetail from "./PropertyDetail";
 
 // CREDIT: Adapted from Code Institute Moments Tutorial Project
 // URL:    https://github.com/Code-Institute-Solutions/moments
@@ -36,9 +38,21 @@ export default function PropertyListPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular Sellers (Mobile)</p>
-        {hasLoaded ? "Property Component" : "Loading Placeholder"}
-
-        <Container>Notes</Container>
+        <Col
+          className={`${appStyles.ContentContainer} pt-4 px-3 px-md-4 rounded`}
+        >
+          {hasLoaded ? (
+            property.results.map((property) => (
+              <PropertyDetail
+                key={property.id}
+                {...property}
+                setProperty={setProperty}
+              />
+            ))
+          ) : (
+            <Asset spinner />
+          )}
+        </Col>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular Seller (Desktop)
