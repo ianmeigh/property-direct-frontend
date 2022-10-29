51c1d1fa068b47492339ae2dcfd044f044425086
ianmeigh <ianmeigh@googlemail.com>
2022-10-29 17:42:34 +0100

    feat: update Asset component to display image

--- src/components/Asset.jsx
+++ src/components/Asset.jsx
@@ -7,7 +7,7 @@ import styles from "../styles/Asset.module.css";
 // CREDIT: Adapted from Code Institute Moments Tutorial Project
 // URL:    https://github.com/Code-Institute-Solutions/moments
 
-const Asset = ({ spinner, upload, message }) => {
+const Asset = ({ spinner, upload, src, message }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
       {spinner && <Spinner animation="border" />}
@@ -16,6 +16,7 @@ const Asset = ({ spinner, upload, message }) => {
           <i className="fas fa-upload fa-3x"></i>
         </span>
       )}
+      {src && <img src={src} alt={message} />}
       {message && <p className="mt-3">{message}</p>}
     </div>
   );
