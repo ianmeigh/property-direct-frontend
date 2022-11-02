b405b13fe013eff44e9a5a572b26623aab0b2116
ianmeigh <ianmeigh@googlemail.com>
2022-11-02 21:10:34 +0000

    feat: add handleFollow function and add to ProfileDataContext provider

--- src/contexts/ProfileDataContext.jsx
+++ src/contexts/ProfileDataContext.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-import { axiosReq } from "../api/axiosDefaults";
+import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "./CurrentUserContext";
 
 // CREDIT: Code Institute Moments Tutorial Project
@@ -19,6 +19,20 @@ export const ProfileDataProvider = ({ children }) => {
   });
 
   const currentUser = useCurrentUser();
+
+  /**
+   * Send request to API to create follower record
+   * @param {Object} clickedProfile - Profile information for user who should be followed
+   */
+  const handleFollow = async (clickedProfile) => {
+    try {
+      const { data } = await axiosRes.post("/followers/", {
+        followed: clickedProfile.owner_id,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
     const handleMount = async () => {
@@ -39,7 +53,7 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={{ setProfileData }}>
+      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
