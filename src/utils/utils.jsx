24dabf4b2f3a57d0b95f2f608f033643ab7f33fa
ianmeigh <ianmeigh@googlemail.com>
2022-11-02 22:26:17 +0000

    feat: add unfollowHelper function

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -32,7 +32,8 @@ export const fetchMoreData = async (resource, setResource) => {
  * @param {Object} profile - User profile.
  * @param {Object} clickedProfile - Profile of the user that is being followed.
  * @param {Int} following_id - ID of the follower record from the API.
- * @returns {Object} - User profile object that has been updated if it was followed.
+ * @returns {Object} - User profile object that has been updated if it was
+ * followed.
  */
 export const followHelper = (profile, clickedProfile, following_id) => {
   return profile.id === clickedProfile.id
@@ -45,6 +46,31 @@ export const followHelper = (profile, clickedProfile, following_id) => {
     ? {
         ...profile,
         following_count: profile.following_count + 1,
+      }
+    : profile;
+};
+
+/**
+ * Conditional logic to determine if the profile passed as a parameter has been
+ * unfollowed, and update the profileData necessary to reflect this action to
+ * the authenticated user.
+ * @param {Object} profile - User profile.
+ * @param {Object} clickedProfile - Profile of the user that is being
+ * unfollowed.
+ * @returns {Object} - User profile object that has been updated if it was
+ * unfollowed.
+ */
+export const unfollowHelper = (profile, clickedProfile) => {
+  return profile.id === clickedProfile.id
+    ? {
+        ...profile,
+        followers_count: profile.followers_count - 1,
+        following_id: null,
+      }
+    : profile.is_owner
+    ? {
+        ...profile,
+        following_count: profile.following_count - 1,
       }
     : profile;
 };
